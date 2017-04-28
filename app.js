62a1a952f051af7b0525c2251a2a1331781a4e36
fjordski <fordheacock@gmail.com>
2017-04-28 11:35:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,34 +2,31 @@ const https = require('https');
 const http = require('http');
 
 function showWeather(city, temp, description) {
-	const message = `The temperature in ${city} is ${temp} degrees. The forecast calls for ${description}`;
-	console.log(message);
+    const message = `The temperature in ${city} is ${temp} degrees. The forecast calls for ${description}`;
+    console.log(message);
 }
 
 function getWeather(zipcode) {
-	try {
+    try {
+	    
+        const request = http.get(`http://samples.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=b2297681910972a2d7aa669cc537e54d`, (res) => {
+            console.log(res.statusCode);
+            let body = "";
 
-	const request = http.get(`http://samples.openweathermap.org/data/2.5/weather?zip=${zipcode},us&appid=b2297681910972a2d7aa669cc537e54d`, (res) => {
-	  console.log(res.statusCode);
-	  	  let body = "";	
+            res.on('data', (data) => {
+                body += data.toString();
+            });
 
-		  res.on('data', (data) => {
-		  	body += data.toString();
+            res.on('end', () => {
+                const weather = JSON.parse(body);
+                showWeather(weather.name, weather.main.temp, weather.weather[0].description);
+            });
+        });
 
-		  });
+        request.on('error', error => console.error(`Problem with message ${error.message}`));
 
-		  res.on('end', () => {
-		   	const weather = JSON.parse(body);
-		   	showWeather(weather.name, weather.main.temp, weather.weather[0].description);
-	  	  });
-	});
-
-	request.on('error', error => console.error(`Problem with message ${error.message}`));
-	
-  	} catch (error) {
-		console.log(error);
-	}
+    } catch (error) {
+        console.log(error);
+    }
 }
-
-
 getWeather(37206);
